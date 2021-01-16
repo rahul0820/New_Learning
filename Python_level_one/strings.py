bbf49ac879aedd8bcc8f46be4f54372b68460684
Rahul Sharma <rah208sh@gmail.com>
2021-01-16 13:16:06 -0500

    Strings

---
+++ Python_level_one/strings.py
@@ -0,0 +1,41 @@
+# mystring = 'abcdefg'
+# print(mystring[0])
+
+# # Negative indexing
+# print(mystring [-1]) 
+
+# # Slicing Begining
+# print(mystring[2:])
+
+# # Slicing Ending
+# print(mystring[:3])
+
+# # Defining Starting and ending point in Slicing
+# print(mystring[2:5])
+
+# # Step size
+# print(mystring[::])
+
+# print(mystring[::2])
+
+# # Basic Methods - Upper
+# x=mystring.upper()
+# print(x)
+
+# # Basic Methods - Split
+
+# x=mystring.split('e')
+# print(x)
+
+# print Formating
+x = "Insert here: {}" .format("another string")
+print(x)
+
+# inserting multipls strings
+x = "Itme one: {}" " Item Two: {}" .format("1","2")
+print(x)
+
+# inserting string with help of defining Variable in .format()
+
+x = "Item one:{a}" " Item two: {b}" .format(a="pen", b="Book")
+print(x)
