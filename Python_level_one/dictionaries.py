3fee9282a7a53d181106a96d4713cca813f5b131
Rahul Sharma <rah208sh@gmail.com>
2021-01-18 14:36:39 -0500

    Dictionaries

---
+++ Python_level_one/dictionaries.py
@@ -0,0 +1,13 @@
+#simple dictionary 
+mystuff = {'Name':'Rahul','City':'Bharatpur'}
+print(mystuff)
+
+# Reassigning
+mystuff={'Name':'Rahul','City':'Bharatpur'}
+mystuff['City']='Pune'
+print(mystuff)
+
+# Adding new key
+mystuff={'Name':'Rahul','City':'Bharatpur'}
+mystuff['Country']='India'
+print(mystuff)
